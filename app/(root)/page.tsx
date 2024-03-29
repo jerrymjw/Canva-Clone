c221c5900f6ee9e7b588dc040a7e4c33763b27ab
Jerry Ma <leilingjerry@gmail.com>
2024-03-29 15:06:21 +1100

    use clerk to authenticate user

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const homePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <p>Home Page</p>
+
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default homePage;
